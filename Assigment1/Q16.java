b8374be2ac528f782a3c986f5498ec71cddfe8f6
Swati Chaudhari <chaudhariswati81296@gmail.com>
2021-05-07 19:31:12 +0530

    Add files via upload

---
+++ Assigment1/Q16.java
@@ -0,0 +1,10 @@
+public class Q16 {
+    public static void main(String[] args)
+    {
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+	}
